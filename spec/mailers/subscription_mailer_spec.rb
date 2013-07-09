c657a84cc0001b880a73c85a88a3843292ae265f
TOAST3R <pabloarean@gmail.com>
2013-07-09 11:20:45 +0200

    disabling automatic mail to user who post a reply

--- spec/mailers/subscription_mailer_spec.rb
+++ spec/mailers/subscription_mailer_spec.rb
@@ -6,15 +6,17 @@ describe Forem::SubscriptionMailer do
     let(:topic) { FactoryGirl.create(:topic) }
     let(:post) { FactoryGirl.create(:post, :topic => topic) }
     let(:mail) { Forem::SubscriptionMailer.topic_reply(post, user) }
-
+# We comment this tests as this option is disabled now (as requested) 
+=begin
   it "sends an email announcing a forum post update" do
     mail.to.should eq([user.email])
     mail.subject.should eq(::I18n.t('received_reply', :scope => 'forem.topic'))
     mail.body.encoded.should match(forum_topic_url(topic.forum, topic))
   end
 
-    it "contains an unsubscribe link" do
-      mail.body.encoded.should match(unsubscribe_forum_topic_path(topic.forum, topic))
-    end
+  it "contains an unsubscribe link" do
+    mail.body.encoded.should match(unsubscribe_forum_topic_path(topic.forum, topic))
+  end
+=end
   end
 end
